9b5faaf9db32fcc079e354118866edb27d21d3ad
mohamed khalil bchir <101249582+Khalil-Bchir@users.noreply.github.com>
2024-02-28 22:11:16 +0100

    update: inclute @fastify/autoload

--- @app/server/src/server.ts
+++ @app/server/src/server.ts
@@ -1,16 +1,13 @@
+import {fastifyAutoload} from '@fastify/autoload'
 import cookie, { FastifyCookieOptions } from '@fastify/cookie';
 import fastifyCors from '@fastify/cors';
 import fjwt, { JWT } from "@fastify/jwt";
 import dotenv from 'dotenv';
 import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 
-import authRoutes from './modules/auth/auth.route';
 import { authSchemas } from './modules/auth/auth.schema';
-import orgRoutes from "./modules/organization/org.route";
 import { orgSchemas } from "./modules/organization/org.schema";
-import userRoutes from './modules/user/user.route';
 import { userSchemas } from "./modules/user/user.schema";
-import userOrgRoute from "./modules/userOrg/userOrg.route";
 import { userOrgSchemas } from "./modules/userOrg/userOrg.schema";
 
 dotenv.config();
@@ -76,10 +73,13 @@ function buildServer() {
         app.addSchema(schema);
     }
 
-    app.register(userRoutes, { prefix: '/api/user' });
-    app.register(orgRoutes, { prefix: '/api/organization' });
-    app.register(userOrgRoute, { prefix: '/api/invitation' });
-    app.register(authRoutes, { prefix: '/api' });
+    app.register(fastifyAutoload , {
+        dir: __dirname + '/modules',
+        options: {
+            prefix: '/api',
+            dirNameRoutePrefix: true,
+        }
+    })
 
     return app;
 }
